d28ac49c73c7c09530244daf880ba9f07266a2da
Mathieu Marchois <mathieu.marchois@gmail.com>
2019-11-25 14:22:50 +0100

    Update dependencies

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
   }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,31 @@
 import {NestFactory} from '@nestjs/core';
+import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import {ValidationPipe} from '@nestjs/common';
 import * as helmet from 'helmet';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   app.use(helmet());
   app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('CoopERP')
+    .setDescription('ERP building for cooperatives')
+    .setVersion('1.0.0')
+    .addBearerAuth('Authorization', 'header')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
