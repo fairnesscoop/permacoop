8965e8230a93e250609732d848ea8b1dfe34e182
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2019-11-29 11:32:32 +0100

    Protect models

--- server/src/Domain/Project/Project.entity.ts
+++ server/src/Domain/Project/Project.entity.ts
@@ -4,18 +4,35 @@ import {Customer} from '../Customer/Customer.entity';
 @Entity()
 export class Project {
   @PrimaryGeneratedColumn('uuid')
-  public id: string;
+  private id: string;
 
   @Column({type: 'varchar', nullable: false})
-  public name: string;
+  private name: string;
 
   @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
-  public createdAt: Date;
+  private createdAt: Date;
 
   @ManyToOne(type => Customer, {nullable: false})
-  public customer: Customer;
+  private customer: Customer;
 
-  constructor(project: Partial<Project>) {
-    Object.assign(this, project);
+  constructor(name: string, customer: Customer) {
+    this.name = name;
+    this.customer = customer;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public getCustomer(): Customer {
+    return this.customer;
   }
 }
