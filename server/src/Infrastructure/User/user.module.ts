a7ebf4da27bc653ff1a127f6bc0b81211d8d9365
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2019-12-02 11:26:33 +0100

    Create User

--- server/src/Infrastructure/User/user.module.ts
+++ server/src/Infrastructure/User/user.module.ts
@@ -3,23 +3,23 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {PassportModule} from '@nestjs/passport';
 import {BusModule} from '../bus.module';
 import {User} from 'src/Domain/User/User.entity';
-import {LoginAction} from './Action/Auth/LoginAction';
-import {RegisterAction} from './Action/Auth/RegisterAction';
+import {LoginAction} from './Action/LoginAction';
+import {CreateUserAction} from './Action/CreateUserAction';
 import {UserRepository} from './Repository/UserRepository';
-import {LoginCommandHandler} from 'src/Application/User/Command/Auth/LoginCommandHandler';
-import {RegisterCommandHandler} from 'src/Application/User/Command/Auth/RegisterCommandHandler';
+import {LoginCommandHandler} from 'src/Application/User/Command/LoginCommandHandler';
+import {CreateUserCommandHandler} from 'src/Application/User/Command/CreateUserCommandHandler';
 import {EncryptionAdapter} from '../Adapter/EncryptionAdapter';
 import {CanRegisterSpecification} from 'src/Domain/User/Specification/CanRegisterSpecification';
 import {BearerStrategy} from './Security/BearerStrategy';
 
 @Module({
   imports: [BusModule, PassportModule, TypeOrmModule.forFeature([User])],
-  controllers: [LoginAction, RegisterAction],
+  controllers: [LoginAction, CreateUserAction],
   providers: [
     {provide: 'IUserRepository', useClass: UserRepository},
     {provide: 'IEncryptionAdapter', useClass: EncryptionAdapter},
     LoginCommandHandler,
-    RegisterCommandHandler,
+    CreateUserCommandHandler,
     CanRegisterSpecification,
     BearerStrategy
   ]
