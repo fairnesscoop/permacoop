9c610b67c2cbbf64ca3bb89610230fdf285fd233
Mathieu Marchois <mathieu.marchois@gmail.com>
2020-04-17 15:49:22 +0200

    [client] Add faircalendar

---
+++ client/src/routes/faircalendar/_Overview.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let overview;
+</script>
+
+<table class="table table-striped table-bordered table-hover mt-3">
+  <thead>
+    <tr>
+      <td class="text-center">Mission</td>
+      <td class="text-center">Support // Podcast</td>
+      <td class="text-center">Dojo</td>
+      <td class="text-center">Formation // Conf // Meetup</td>
+      <td class="text-center">Ferié</td>
+      <td class="text-center">Vacances</td>
+      <td class="text-center">Congé maladie</td>
+      <td class="text-center">Autres</td>
+      <td class="text-center">Tickets Resto</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="text-center">{overview.mission}</td>
+      <td class="text-center">{overview.support}</td>
+      <td class="text-center">{overview.dojo}</td>
+      <td class="text-center">{overview.formationConference}</td>
+      <td class="text-center">{overview.workFree}</td>
+      <td class="text-center">{overview.holiday}</td>
+      <td class="text-center">{overview.medicalLeave}</td>
+      <td class="text-center">{overview.other}</td>
+      <td class="text-center">
+        <b>{overview.mealTicket}</b>
+      </td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <th colspan="9">Total jour(s) travaillé(s): {overview.totalTimeSpent}</th>
+    </tr>
+  </tfoot>
+</table>
---
+++ client/src/routes/faircalendar/index.svelte
@@ -0,0 +1,93 @@
+<script context="module">
+  export const preload = ({query}) => {
+    return {
+      filters: {
+        date: query.date ? new Date(query.date) : new Date(),
+        userId: query.userId ? query.userId : null
+      }
+    };
+  };
+</script>
+
+<script>
+  import {onMount} from 'svelte';
+  import frLocale from '@fullcalendar/core/locales/fr';
+  import '@fullcalendar/core/main.css';
+  import '@fullcalendar/daygrid/main.css';
+  import {user} from '../../store';
+  import {client as axios} from '../../utils/axios';
+  import Filters from './_Filters.svelte';
+  import Overview from './_Overview.svelte';
+  import {errorNormalizer} from '../../normalizer/errors';
+  import Breadcrumb from '../_components/Breadcrumb.svelte';
+  import Loader from '../_components/Loader.svelte';
+  import ServerErrors from '../_components/ServerErrors.svelte';
+
+  export let filters;
+
+  let loading = false;
+  let errors = [];
+  let data = {};
+  let isLoggedUser = false;
+
+  const fullCalendar = async (events, date) => {
+    const {Calendar} = await import('@fullcalendar/core');
+    const {default: dayGridPlugin} = await import('@fullcalendar/daygrid');
+    const dom = document.getElementById('calendar');
+    dom.innerHTML = '';
+    const calendar = new Calendar(dom, {
+      locale: frLocale,
+      plugins: [dayGridPlugin],
+      nowIndicator: true,
+      height: 600,
+      header: {left: 'title', center: '', right: ''},
+      columnHeaderFormat: {weekday: 'long'},
+      events: events,
+      businessHours: {
+        daysOfWeek: [1, 2, 3, 4, 5]
+      }
+    });
+    calendar.gotoDate(date);
+    calendar.render();
+  };
+
+  const fetchEvents = async params => {
+    try {
+      loading = true;
+      isLoggedUser = params.userId === $user.id;
+      ({data} = await axios.get('events', {params}));
+      fullCalendar(data.events, params.date);
+    } catch (e) {
+      errors = errorNormalizer(e);
+    } finally {
+      loading = false;
+    }
+  };
+
+  onMount(async () => {
+    if (!filters.userId) {
+      filters.userId = $user.id;
+    }
+
+    fetchEvents(filters);
+  });
+
+  const onFilter = e => {
+    fetchEvents(e.detail);
+  };
+</script>
+
+<svelte:head>
+  <title>Permacoop - FairCalendar</title>
+</svelte:head>
+
+<div class="col-md-12">
+  <Breadcrumb items={[{title: 'FairCalendar'}]} />
+  <ServerErrors {errors} />
+  <Filters {...filters} on:filter={onFilter} />
+  <Loader {loading} />
+  <div id="calendar" />
+  {#if data.overview}
+    <Overview overview={data.overview} />
+  {/if}
+</div>
