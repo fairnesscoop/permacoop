54d9be25518aa673f23e9b8a7fb1f0d97b40f8ea
Mathieu Marchois <mathieu.marchois@gmail.com>
2020-04-24 11:50:05 +0200

    List quotes

--- server/src/Domain/Accounting/Repository/IQuoteRepository.ts
+++ server/src/Domain/Accounting/Repository/IQuoteRepository.ts
@@ -3,5 +3,6 @@ import {Quote} from '../Quote.entity';
 export interface IQuoteRepository {
   save(quote: Quote): Promise<Quote>;
   find(id: string): Promise<Quote>;
+  findAll(): Promise<Quote[]>;
   countByYear(year: number): Promise<number>;
 }
