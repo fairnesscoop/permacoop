92835a50db849349c58bd4f368f98349218ba4c0
Gregory Pelletier <gpelletier@ip512.com>
2020-11-15 11:53:20 +0100

    add test to cover no leave and no moderator cases

--- server/src/Application/HumanResource/Leave/Query/GetLeaveRequestByIdQueryHandler.spec.ts
+++ server/src/Application/HumanResource/Leave/Query/GetLeaveRequestByIdQueryHandler.spec.ts
@@ -1,16 +1,15 @@
-import { mock, instance, when, verify, deepEqual } from 'ts-mockito';
-import { DateUtilsAdapter } from 'src/Infrastructure/Adapter/DateUtilsAdapter';
-import { LeaveRequestView } from '../View/LeaveRequestView';
+import { LeaveRequestNotFoundException } from 'src/Domain/HumanResource/Leave/Exception/LeaveRequestNotFoundException';
 import {
-  Type,
-  Status,
-  LeaveRequest
+  LeaveRequest, Status, Type
 } from 'src/Domain/HumanResource/Leave/LeaveRequest.entity';
-import { UserSummaryView } from '../../User/View/UserSummaryView';
 import { User } from 'src/Domain/HumanResource/User/User.entity';
+import { DateUtilsAdapter } from 'src/Infrastructure/Adapter/DateUtilsAdapter';
 import { LeaveRequestRepository } from 'src/Infrastructure/HumanResource/Leave/Repository/LeaveRequestRepository';
-import { GetLeaveRequestByIdQueryHandler } from './GetLeaveRequestByIdQueryHandler';
+import { instance, mock, verify, when } from 'ts-mockito';
+import { UserSummaryView } from '../../User/View/UserSummaryView';
+import { LeaveRequestView } from '../View/LeaveRequestView';
 import { GetLeaveRequestByIdQuery } from './GetLeaveRequestByIdQuery';
+import { GetLeaveRequestByIdQueryHandler } from './GetLeaveRequestByIdQueryHandler';
 
 describe('GetLeaveRequestByIdQueryHandler', () => {
   let leaveRequestRepository: LeaveRequestRepository;
@@ -71,17 +70,103 @@ describe('GetLeaveRequestByIdQueryHandler', () => {
     when(leave.getModerator()).thenReturn(instance(moderator));
     when(leave.getModerationComment()).thenReturn('Go go go');
 
-    when(leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')).thenResolve(instance(leave));
+    when(
+      leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')
+    ).thenResolve(instance(leave));
 
     when(
       dateUtils.getLeaveDuration('2020-05-05', false, '2020-05-15', true)
     ).thenReturn(7.5);
 
-    expect(await queryHandler.execute(new GetLeaveRequestByIdQuery('204522d3-f077-4d21-b3ee-6e0d742fca44'))).toMatchObject(
-      expectedResult
+    expect(
+      await queryHandler.execute(
+        new GetLeaveRequestByIdQuery('204522d3-f077-4d21-b3ee-6e0d742fca44')
+      )
+    ).toMatchObject(expectedResult);
+
+    verify(
+      dateUtils.getLeaveDuration('2020-05-05', false, '2020-05-15', true)
+    ).once();
+    verify(
+      leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')
+    ).once();
+  });
+
+  it('testLeaveNotFound', async () => {
+    when(
+      leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')
+    ).thenResolve(null);
+
+    try {
+      await queryHandler.execute(
+        new GetLeaveRequestByIdQuery('204522d3-f077-4d21-b3ee-6e0d742fca44')
+      );
+    } catch (e) {
+      expect(e).toBeInstanceOf(LeaveRequestNotFoundException);
+      expect(e.message).toBe(
+        'human_resources.leaves.requests.errors.not_found'
+      );
+    }
+    verify(
+      leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')
+    ).once();
+  });
+
+  it('testLeaveWithoutModerator', async () => {
+    const expectedResult = new LeaveRequestView(
+      'a3753b9c-b711-4e0e-a535-e473161bd612',
+      Type.PAID,
+      Status.ACCEPTED,
+      '2020-05-05',
+      '2020-05-15',
+      7.5,
+      'Country vacation',
+      new UserSummaryView(
+        '2402455a-4dc1-47c9-89a4-f9b859f02f5c',
+        'John',
+        'DOE'
+      ),
+      null,
+      null
     );
 
-    verify(dateUtils.getLeaveDuration('2020-05-05', false, '2020-05-15', true)).once();
-    verify(leaveRequestRepository.findOneById('204522d3-f077-4d21-b3ee-6e0d742fca44')).once();
+    const user = mock(User);
+    when(user.getId()).thenReturn('2402455a-4dc1-47c9-89a4-f9b859f02f5c');
+    when(user.getFirstName()).thenReturn('John');
+    when(user.getLastName()).thenReturn('DOE');
+
+    const leave = mock(LeaveRequest);
+    when(leave.getId()).thenReturn('a3753b9c-b711-4e0e-a535-e473161bd612');
+    when(leave.getType()).thenReturn(Type.PAID);
+    when(leave.getStatus()).thenReturn(Status.ACCEPTED);
+    when(leave.getStartDate()).thenReturn('2020-05-05');
+    when(leave.isStartsAllDay()).thenReturn(false);
+    when(leave.getEndDate()).thenReturn('2020-05-15');
+    when(leave.isEndsAllDay()).thenReturn(true);
+    when(leave.getComment()).thenReturn('Country vacation');
+    when(leave.getUser()).thenReturn(instance(user));
+    when(leave.getModerator()).thenReturn(null);
+    when(leave.getModerationComment()).thenReturn(null);
+
+    when(
+      leaveRequestRepository.findOneById('a3753b9c-b711-4e0e-a535-e473161bd612')
+    ).thenResolve(instance(leave));
+
+    when(
+      dateUtils.getLeaveDuration('2020-05-05', false, '2020-05-15', true)
+    ).thenReturn(7.5);
+
+    expect(
+      await queryHandler.execute(
+        new GetLeaveRequestByIdQuery('a3753b9c-b711-4e0e-a535-e473161bd612')
+      )
+    ).toMatchObject(expectedResult);
+
+    verify(
+      dateUtils.getLeaveDuration('2020-05-05', false, '2020-05-15', true)
+    ).once();
+    verify(
+      leaveRequestRepository.findOneById('a3753b9c-b711-4e0e-a535-e473161bd612')
+    ).once();
   });
 });
