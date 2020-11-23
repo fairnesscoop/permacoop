d94a900e895dbef1ee150d34e2e691478b6cf64f
Grégory Pelletier <gpelletier@macbook-pro-de-gregory.home>
2020-11-23 22:04:51 +0100

    improve test coverage

--- server/src/Domain/HumanResource/User/UserAdministrative.entity.spec.ts
+++ server/src/Domain/HumanResource/User/UserAdministrative.entity.spec.ts
@@ -9,7 +9,7 @@ describe('UserAdministrative.entity', () => {
       ContractType.CDD,
       '2020-01-01',
       '2021-01-01',
-      75500
+      7550
     );
 
     expect(admin.getId()).toBe(undefined);
@@ -17,8 +17,37 @@ describe('UserAdministrative.entity', () => {
     expect(admin.getContract()).toBe(ContractType.CDD);
     expect(admin.getJoiningDate()).toBe('2020-01-01');
     expect(admin.getLeavingDate()).toBe('2021-01-01');
-    expect(admin.getTransportFee()).toBe(75500);
+    expect(admin.getTransportFee()).toBe(7550);
     expect(admin.isExecutivePosition()).toBe(true);
     expect(admin.haveHealthInsurance()).toBe(true);
+  });
+
+  it('testUpdate', () => {
+    const admin = new UserAdministrative(
+      5000000,
+      true,
+      true,
+      ContractType.CDD,
+      '2020-01-01',
+      null,
+      7550
+    );
+
+    admin.update(
+      3000000,
+      ContractType.APPRENTICESHIP,
+      false,
+      false,
+      '2020-01-02',
+      '2021-01-02',
+      null
+    );
+    expect(admin.getAnnualEarnings()).toBe(3000000);
+    expect(admin.getContract()).toBe(ContractType.APPRENTICESHIP);
+    expect(admin.isExecutivePosition()).toBe(false);
+    expect(admin.haveHealthInsurance()).toBe(false);
+    expect(admin.getJoiningDate()).toBe('2020-01-02');
+    expect(admin.getLeavingDate()).toBe('2021-01-02');
+    expect(admin.getTransportFee()).toBe(null);
   });
 });
