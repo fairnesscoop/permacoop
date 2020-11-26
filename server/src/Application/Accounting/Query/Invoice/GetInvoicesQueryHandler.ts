344176e514af6df769703d62df974c7745ce3750
Mathieu Marchois <mathieu.marchois@gmail.com>
2020-11-26 17:39:59 +0100

    Improve quality code

--- server/src/Application/Accounting/Query/Invoice/GetInvoicesQueryHandler.ts
+++ server/src/Application/Accounting/Query/Invoice/GetInvoicesQueryHandler.ts
@@ -6,6 +6,7 @@ import { IInvoiceRepository } from 'src/Domain/Accounting/Repository/IInvoiceRep
 import { InvoiceView } from '../../View/DailyRate/InvoiceView';
 import { ProjectView } from 'src/Application/Project/View/ProjectView';
 import { CustomerView } from 'src/Application/Customer/View/CustomerView';
+import { InvoiceItem } from 'src/Domain/Accounting/InvoiceItem.entity';
 
 @QueryHandler(GetInvoicesQuery)
 export class GetInvoicesQueryHandler {
@@ -23,20 +24,7 @@ export class GetInvoicesQueryHandler {
     for (const invoice of invoices) {
       const project = invoice.getProject();
       const customer = project.getCustomer();
-
-      let amountExcludingVat = 0;
-
-      for (const item of invoice.getItems()) {
-        const amount = item.getAmount() / 100;
-        const quantity = item.getQuantity() / 100;
-        let totalAmount = amount * quantity;
-
-        if (item.getDiscount() > 0) {
-          totalAmount *= (item.getDiscount() / 10000);
-        }
-
-        amountExcludingVat += totalAmount;
-      }
+      const amountExcludingVat = this.calculateAmountExcludingVat(invoice.getItems());
 
       results.push(
         new InvoiceView(
@@ -58,5 +46,23 @@ export class GetInvoicesQueryHandler {
     }
 
     return new Pagination<InvoiceView>(results, total);
+  }
+
+  private calculateAmountExcludingVat(items: InvoiceItem[]): number {
+    let amountExcludingVat = 0;
+
+    for (const item of items) {
+      const amount = item.getAmount() / 100;
+      const quantity = item.getQuantity() / 100;
+      let totalAmount = amount * quantity;
+
+      if (item.getDiscount() > 0) {
+        totalAmount *= (item.getDiscount() / 10000);
+      }
+
+      amountExcludingVat += totalAmount;
+    }
+
+    return amountExcludingVat;
   }
 }
