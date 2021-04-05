4ac682cd04c9783709515ab0acd979a53352ef4f
BertrandD <volubyl@pm.me>
2021-04-05 10:00:19 +0200

    meal-tickets display summary

--- client/src/routes/human_resources/meal_tickets/index.svelte
+++ client/src/routes/human_resources/meal_tickets/index.svelte
@@ -2,8 +2,27 @@
   import Breadcrumb from '../../../components/Breadcrumb.svelte';
   import { _ } from 'svelte-i18n';
   import H4Title from '../../../components/H4Title.svelte';
-
+  import { onMount } from 'svelte';
+  import { errorNormalizer } from '../../../normalizer/errors';
+  import ServerErrors from '../../../components/ServerErrors.svelte';
+  import { get } from '../../../utils/axios';
+  import Table from './_Table.svelte';
   let title = $_('human_resources.meal_tickets.title');
+
+  let mealTicketsSummaries = [];
+  let errors;
+
+  const fetchMealTicketsSummaries = async () => {
+    try {
+      mealTicketsSummaries = (await get('meal-tickets/count')).data;
+    } catch (e) {
+      errors = errorNormalizer(e);
+    }
+  };
+
+  onMount(async () => {
+    await fetchMealTicketsSummaries();
+  });
 </script>
 
 <svelte:head>
@@ -14,14 +33,5 @@
   items="{[{ title: $_('human_resources.breadcrumb') }, { title }]}" />
 <H4Title title="{$_('human_resources.meal_tickets.available_meal_tickets')}" />
 
-<table class="w-full whitespace-no-wrap">
-  <thead>
-    <tr
-      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-      <th class="px-4 py-3">{$_('human_resources.meal_tickets.month')}</th>
-      <th class="px-4 py-3">{$_('human_resources.meal_tickets.base')}</th>
-      <th class="px-4 py-3">{$_('human_resources.meal_tickets.exceptions')}</th>
-      <th class="px-4 py-3">{$_('human_resources.meal_tickets.total')}</th>
-    </tr>
-  </thead>
-</table>
+<ServerErrors errors="{errors}" />
+<Table mealTicketsSummaries="{mealTicketsSummaries}" />
