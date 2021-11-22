fa9f1c27474ee727adfe8c46fc8a8c5e3056e816
florimondmanca <florimond.manca@protonmail.com>
2021-11-22 13:30:25 +0100

    Show payroll elements period on button

--- client/src/routes/human_resources/pay_slips/_PayrollButton.svelte
+++ client/src/routes/human_resources/pay_slips/_PayrollButton.svelte
@@ -1,12 +1,15 @@
 <script>
   import { _ } from 'svelte-i18n';
+  import { format } from 'date-fns';
 
   export let disabled;
+
+  const period = format(new Date(), 'MMM y');
 </script>
 
 <button
   {disabled}
   on:click
   class="ml-2 mb-6 px-2 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-  {$_('human_resources.pay_slips.download_payroll_elements')}
+  {$_('human_resources.pay_slips.elements.download', { values: { period } })}
 </button>
