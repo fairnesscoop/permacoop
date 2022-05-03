f37313de7e1be42e545ad03bf294e22a55195e85
mmarchois <mathieu.marchois@gmail.com>
2022-05-03 15:47:53 +0200

    Change daily rates order

--- client/src/routes/accounting/daily_rates/_Table.svelte
+++ client/src/routes/accounting/daily_rates/_Table.svelte
@@ -11,8 +11,8 @@
     <tr
       class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
       <th class="px-4 py-3">{$_('accounting.daily_rates.customer')}</th>
-      <th class="px-4 py-3">{$_('accounting.daily_rates.user')}</th>
       <th class="px-4 py-3">{$_('accounting.daily_rates.task')}</th>
+      <th class="px-4 py-3">{$_('accounting.daily_rates.user')}</th>
       <th class="px-4 py-3">{$_('accounting.daily_rates.amount')}</th>
       <th class="px-4 py-3">{$_('common.actions')}</th>
     </tr>
@@ -21,8 +21,8 @@
     {#each items as { id, user, task, customer, amount } (id)}
       <tr class="text-gray-700 dark:text-gray-400">
         <td class="px-4 py-3 text-sm">{customer.name}</td>
-        <td class="px-4 py-3 text-sm">{user.firstName} {user.lastName}</td>
         <td class="px-4 py-3 text-sm">{task.name}</td>
+        <td class="px-4 py-3 text-sm">{user.firstName} {user.lastName}</td>
         <td class="px-4 py-3 text-sm">{format(amount)}</td>
         <td class="px-4 py-3">
           <div class="flex items-center space-x-4 text-sm">
