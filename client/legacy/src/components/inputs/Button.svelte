6922671371a3db7fbed4760c2caf4259b36d900b
benpaquier <hello@benoitpaquier.com>
2023-01-18 12:34:41 +0100

    Add default name to button, add style when disabled

--- client/legacy/src/components/inputs/Button.svelte
+++ client/legacy/src/components/inputs/Button.svelte
@@ -1,11 +1,11 @@
 <script>
   export let loading;
   export let disabled = false;
-  export let name;
+  export let name = 'submit';
   export let value;
 
   const className =
-    'block w-full px-4 py-2 mt-6 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple';
+    `block w-full px-4 py-2 mt-6 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple ${disabled && 'opacity-50 cursor-auto'}`;
 </script>
 
 <button {name} type="submit" class={className} disabled={loading || disabled}>
