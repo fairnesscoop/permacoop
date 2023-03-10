d9104ae8f8b3ecee6ba6155cec519c8ec53ba964
Antoine Smagghe <antoine.smagghe@fairness.coop>
2023-03-10 15:36:38 +0100

    Remove invoices server side

--- server/src/Domain/Accounting/Repository/IInvoiceItemRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Invoice } from '../Invoice.entity';
-import { InvoiceItem } from '../InvoiceItem.entity';
-
-export interface IInvoiceItemRepository {
-  save(items: InvoiceItem[]): void;
-}
