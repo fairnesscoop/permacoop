ef4b615c59b4e9aea8753c3bbe99c88e83604dfb
benpaquier <hello@benoitpaquier.com>
2023-03-15 16:24:31 +0100

    Complete GetLeaveRequestsDTO specs

--- server/src/Infrastructure/HumanResource/Leave/DTO/GetLeaveRequestsDTO.spec.ts
+++ server/src/Infrastructure/HumanResource/Leave/DTO/GetLeaveRequestsDTO.spec.ts
@@ -12,4 +12,23 @@ describe('GetLeaveRequestsDTO', () => {
     const validation = await validate(dto);
     expect(validation).toHaveLength(0);
   });
+
+  it('testInvalidDto', async () => {
+    const dto = new GetLeaveRequestsDTO();
+    dto.page = -1;
+    dto.limit = -10;
+    dto.status = Status.ACCEPTED;
+
+    const validation = await validate(dto);
+
+    expect(validation).toHaveLength(2);
+
+    expect(validation[0].constraints).toMatchObject({
+      isPositive: 'limit must be a positive number'
+    });
+
+    expect(validation[1].constraints).toMatchObject({
+      min: 'page must not be less than 1'
+    });
+  });
 });
