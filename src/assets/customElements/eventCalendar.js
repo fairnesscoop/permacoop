30e230640a3900958da6c9aefc41d84bede94e31
florimondmanca <florimond.manca@fairness.coop>
2023-11-17 17:28:35 +0100

    Add faircalendar e2e test, fix calendar caching

--- src/assets/customElements/eventCalendar.js
+++ src/assets/customElements/eventCalendar.js
@@ -1,24 +1,53 @@
+// @ts-check
+import * as Turbo from '@hotwired/turbo';
 import Calendar from '@event-calendar/core';
 import DayGrid from '@event-calendar/day-grid';
 import Interaction from '@event-calendar/interaction';
 import { format, subDays } from 'date-fns';
 
 export default class extends HTMLElement {
+  /** @type {Calendar} */
+  #ec;
+
+  /** @type{string} */
+  #addUrlTemplate;
+
   connectedCallback() {
-    this.classList.add('pc-eventcalendar');
+    this.dataset.testid = 'pc-event-calendar';
 
-    const events = JSON.parse(this.dataset.eventsJson);
+    const events = JSON.parse(this.dataset.eventsJson || '{}');
     const addUrlTemplate = this.dataset.addUrlTemplate;
+    const date = this.dataset.date || new Date('now').toISOString();
 
-    const goToEventCreate = (startDate, endDate) => {
-      const url = addUrlTemplate
-        .replace(':startDate', format(startDate, 'yyyy-MM-dd'))
-        .replace(':endDate', format(endDate, 'yyyy-MM-dd'));
+    if (!addUrlTemplate) {
+      throw new Error('data-add-url-template is missing');
+    }
 
-      window.location = url;
-    };
+    this.#addUrlTemplate = addUrlTemplate;
+    this.#ec = this.#createCalendar(date, events);
 
-    const ec = new Calendar({
+    document.addEventListener('turbo:before-cache', this.#onTurboBeforeCache);
+  }
+
+  /**
+   * @param {Date} startDate
+   * @param {Date} endDate
+   */
+  #goToEventCreate = (startDate, endDate) => {
+    const url = this.#addUrlTemplate
+      .replace(':startDate', format(startDate, 'yyyy-MM-dd'))
+      .replace(':endDate', format(endDate, 'yyyy-MM-dd'));
+
+    Turbo.visit(url);
+  };
+
+  /**
+   * @param {string} date
+   * @param {any[]} events
+   * @returns {Calendar}
+   */
+  #createCalendar = (date, events) => {
+    return new Calendar({
       target: this,
       props: {
         plugins: [DayGrid, Interaction],
@@ -30,30 +59,42 @@ export default class extends HTMLElement {
             6, // Saturday
             0 // Sunday
           ],
-          date: this.dataset.date,
+          date,
           headerToolbar: { start: '', center: '', end: '' },
           dayMaxEvents: true,
           eventStartEditable: false,
           eventDurationEditable: false,
+          // TODO: add testid on days
           eventContent: ({ event }) => {
             const url = event.extendedProps.url;
             if (url) {
-              return { html: `<a href="${url}">${event.title}</a>` };
+              return {
+                html: `<a href="${url}" data-turbo-frame="_top">${event.title}</a>`
+              };
             }
             return event.title;
           },
-          dateClick: info => goToEventCreate(info.date, info.date),
+          dateClick: ({ event, date }) => {
+            this.#goToEventCreate(date, date);
+          },
           selectable: true,
           selectBackgroundColor: 'var(--background-action-violet)',
-          select: info => {
-            // By default, range will stay selected if navigating using the
-            // back button.
-            ec.unselect();
-
-            goToEventCreate(info.start, subDays(info.end, 1));
+          select: ({ start, end }) => {
+            this.#goToEventCreate(start, subDays(end, 1));
           }
         }
       }
     });
+  };
+
+  #onTurboBeforeCache = () => {
+    this.#ec.destroy();
+  };
+
+  disconnectedCallback() {
+    document.removeEventListener(
+      'turbo:before-cache',
+      this.#onTurboBeforeCache
+    );
   }
 }
