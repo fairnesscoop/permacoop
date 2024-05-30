e78ef2dcdabd9fe0288ee642abfd0640168b3887
Gregory <gregory.pelletier@fairness.coop>
2024-05-30 18:33:54 +0200

    Update src/Infrastructure/Common/DTO/PaginationDTO.ts
    Co-authored-by: Florimond Manca <florimond.manca@fairness.coop>

--- src/Infrastructure/Common/DTO/PaginationDTO.ts
+++ src/Infrastructure/Common/DTO/PaginationDTO.ts
@@ -1,11 +1,11 @@
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationDTO {
   @IsOptional()
-  @Type(() => Number)
   @Min(1)
   @Max(10000)
   @IsNumber()
+  @Transform((_, { page }) => +page)
   public page = 1;
 }
