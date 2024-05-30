4c1e68e52f74fb771f7962ec31c565e793734278
Gregory Pelletier <gregory.pelletier@fairness.coop>
2024-05-30 08:41:29 +0200

    fix: convert page query param to number

--- src/Infrastructure/Common/DTO/PaginationDTO.ts
+++ src/Infrastructure/Common/DTO/PaginationDTO.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationDTO {
   @IsOptional()
+  @Type(() => Number)
   @Min(1)
   @Max(10000)
   @IsNumber()
